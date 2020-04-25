624e5bdd359f5de8de25b68479cd1542eed26082
Zhaopinglu <zhaopinglu77@gmail.com>
2020-04-25 16:01:30 +0800

    update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,5 +1,5 @@
 REM Get compose file:
-REM Note: You might need proxy to access below file.
+REM Note: Might need proxy to access below file.
 wget https://raw.githubusercontent.com/zhaopinglu/docker-spark/master/docker-compose-spark-hadoop.yml -O docker-compose-spark-hadoop.yml
 
 REM Start up
@@ -9,3 +9,4 @@ docker-compose -f docker-compose-spark-hadoop.yaml up -d
 REM Shut down
 docker-compose -f docker-compose-spark-hadoop.yaml down
 docker network rm spark-net
+
