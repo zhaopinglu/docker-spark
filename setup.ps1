345198b99728995b3f9ec90a359a9047370ed0bf
Zhaopinglu <zhaopinglu77@gmail.com>
2020-04-25 17:03:42 +0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,12 @@
-REM Note: Might need proxy to access below file.
+# Note: Might need proxy to access below file.
 wget https://raw.githubusercontent.com/zhaopinglu/docker-spark/master/docker-compose-spark-hadoop.yml -O docker-compose-spark-hadoop.yml
 
-REM Start up
-docker network create spark-net
+# Start up
+docker network create spark-net -d nat
 docker-compose -f docker-compose-spark-hadoop.yml up -d
 
-REM Shut down
-REM docker-compose -f docker-compose-spark-hadoop.yml down
-REM docker network rm spark-net
+# Shut down
+# docker-compose -f docker-compose-spark-hadoop.yml down
+# docker-compose -f docker-compose-spark-hadoop.yml rm
+# docker network rm spark-net
 
