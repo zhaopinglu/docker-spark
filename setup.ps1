7752fd04bc0fde31a355011fc422089a2886f815
Zhaopinglu <zhaopinglu77@gmail.com>
2020-04-26 14:29:00 +0800

    update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,16 @@
 # Note: Might need proxy to access below file.
-wget https://raw.githubusercontent.com/zhaopinglu/docker-spark/master/docker-compose-spark-hadoop.yml -O docker-compose-spark-hadoop.yml
+wget https://raw.githubusercontent.com/zhaopinglu/docker-spark/master/docker-compose.yml -O docker-compose.yml
 
-docker network rm spark-net
-docker network create -d bridge --subnet=192.168.3.0/24 --gateway 192.168.3.1 -o com.docker.network.bridge.enable_icc=true -o com.docker.network.bridge.enable_ip_masquerade=true spark-net
+#docker network rm spark-net
+
+# Let docker compser do this job
+#docker network create -d bridge --subnet=192.168.3.0/24 --gateway 192.168.3.1 -o com.docker.network.bridge.enable_icc=true -o com.docker.network.bridge.enable_ip_masquerade=true spark-net
 
 # Start up
-docker-compose -f docker-compose-spark-hadoop.yml up -d
+docker-compose up -d
 
 # Shut down
-# docker-compose -f docker-compose-spark-hadoop.yml down
-# docker-compose -f docker-compose-spark-hadoop.yml rm
+# docker-compose -f docker-compose.yml down
+# docker-compose -f docker-compose.yml rm
 # docker network rm spark-net
 
